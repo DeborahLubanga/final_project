e70ff04d5b070ddfa6d5952ef58f29d18bd7fb43
DeborahLubanga <dnl237@nyu.edu>
2016-04-27 21:51:11 -0400

    Making the Loading Screen Work

--- js/loading.js
+++ js/loading.js
@@ -2,15 +2,23 @@ var welcomeBtn = document.getElementById('welcomebutton');
 
 var welcomeScreen = document.getElementById('welcomeScreen');
 
+var endLoading = document.getElementById('endLoading');
+
 //Function to make it go away
 
 function entering(){
 	welcomeScreen.className = 'enter';	
 }
 
+function endingLoading (){
+	endLoading.style.zIndex = "-100";
+}
+
 
 //event listener
 
 welcomeBtn.addEventListener('click', entering);
+
+endLoading.addEventListener("animationend", endingLoading)
 
 
