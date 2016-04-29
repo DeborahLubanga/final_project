96458325728eed64f75a4963d5b5157dc13075bc
DeborahLubanga <dnl237@nyu.edu>
2016-04-28 21:53:34 -0400

    Setting up loading page greeting

--- js/loading.js
+++ js/loading.js
@@ -4,7 +4,30 @@ var welcomeScreen = document.getElementById('welcomeScreen');
 
 var endLoading = document.getElementById('endLoading');
 
-//Function to make it go away
+var x =  Math.floor(Math.random() * 360);
+//var y =  Math.floor(Math.random() * 360);
+//var z =  Math.floor(Math.random() * 360);
+
+var textColor = [x, 0, 0];
+
+
+//Creating Canvas
+function setup(){
+	var myCanvas = createCanvas(1000,500);
+	myCanvas.parent('endLoading');
+}
+
+function draw(){
+	textSize(150);
+	textAlign(CENTER, CENTER);
+	textFont("Helevetic");
+	var welcome = "WELCOME";
+	fill(textColor);
+	text(welcome, canvas.width/2,  canvas.height/2);
+	
+}
+
+//Function to make loading screen go away
 
 function entering(){
 	welcomeScreen.className = 'enter';	
@@ -15,7 +38,7 @@ function endingLoading (){
 }
 
 
-//event listener
+//event listeners
 
 welcomeBtn.addEventListener('click', entering);
 
